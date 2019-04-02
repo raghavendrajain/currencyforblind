aa1993790f053a03bc85a3a800dcc99694ac35ba
mondher-bouazizi <mondher.bouazizi@gmail.com>
2019-04-02 15:01:35 +0900

    one click analyze and read

--- app/static/client.js
+++ app/static/client.js
@@ -25,6 +25,7 @@ function analyze() {
         if (this.readyState === 4) {
             var response = JSON.parse(e.target.responseText);
             el('result-label').innerHTML = `These are ${response['result']} rupees`;
+            responsiveVoice.speak($('#result-label').text());
         }
         el('analyze-button').innerHTML = 'Analyze';
     }
