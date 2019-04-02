4ec16bcaea3c46fee0bc8f3dd74decdb538ab9b5
mondher-bouazizi <mondher.bouazizi@gmail.com>
2019-04-02 16:27:14 +0900

    Test_3

--- app/static/client.js
+++ app/static/client.js
@@ -26,7 +26,23 @@ function analyze() {
             var response = JSON.parse(e.target.responseText);
             el('result-label').innerHTML = `These are ${response['result']} rupees`;
             
+            
+            // ==================
+            
+            var resources = ["https://code.responsivevoice.org/responsivevoice.js", "https://code.jquery.com/jquery-2.1.4.min.js"];
+            
+            for(var i = 0; i < resources.length; i++){
+                var script = document.createElement("script");
+                script.setAttribute("type", "text/javascript");
+                script.setAttribute("src", resources[i]);
+                document.getElementsByTagName("head")[0].appendChild(script);
+            }
+
             responsiveVoice.speak("These are " + ${response['result']} + " rupees", "US English Female");
+           
+            // ==================
+            
+            
         }
         el('analyze-button').innerHTML = 'Analyze';
     }
